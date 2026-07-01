4b3d41cf207f7678c66ed3e1aef888041b708d87
Kairav <kairavdutta@gmail.com>
2026-07-01 14:08:29 +0530

    added copywrite header in logger.h and logger.c

--- Logger.cpp
+++ Logger.cpp
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2026, Kairav Dutta (@ka1rav6)
+
+This is free and unencumbered software released into the public domain,
+except that the above copyright notice must be retained in all copies
+of this software, in source or binary form.  That's the only requirement.
+*/
+
 #include "Logger.h"
 
 std::mutex Logger::LogStream::m_mutex;
--- Logger.h
+++ Logger.h
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2026, Kairav Dutta (@ka1rav6)
+
+This is free and unencumbered software released into the public domain,
+except that the above copyright notice must be retained in all copies
+of this software, in source or binary form.  That's the only requirement.
+*/
+
 #ifndef LOGGER_H
 #define LOGGER_H
 
